5d39f4be0f9e72954cfe57faaba8f70de283a787
Alex Kozlov <62183294+alexkozlovm@users.noreply.github.com>
2025-04-20 14:14:19 -0400

    123

--- scripts/library.js
+++ scripts/library.js
@@ -12,7 +12,10 @@ function addBookToLibrary(title, author) {
 
 addBookToLibrary("book", "alex");
 
-function display() {
-    document.getElementById("r1c1").innerHTML = "I need the value of "
+for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
 }
 
+const element = document.getElementById('r1c1');
+const htmlContent = element.innerHTML;
+console.log(htmlContent);
