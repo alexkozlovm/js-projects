ab536a94fff5ec872c86af9dba4db1a95b60c866
Alex Kozlov <62183294+alexkozlovm@users.noreply.github.com>
2025-04-20 13:41:04 -0400

    test 1

---
+++ scripts/library.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function Book(title, author) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+}
+
+function addBookToLibrary(title, author) {
+    myLibrary.push(Book(title, author));
+}
+
+for (let i = 0; i < myLibrary.length; i++) {
+    console.log(myLibrary[i]);
+}
